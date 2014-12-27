0eb120c90491f33d49f195581d6b3f37fc3349bd
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-27 09:05:15 -0300

    Add system definition and parser

---
+++ parenml.asd
@@ -0,0 +1,16 @@
+(defsystem parenml
+  :author "Fernando Borretti <eudoxiahp@gmail.com>"
+  :maintainer "Fernando Borretti <eudoxiahp@gmail.com>"
+  :license "MIT"
+  :version "0.1"
+  :depends-on (:common-doc
+               :esrap)
+  :components ((:module "src"
+                :serial t
+                :components
+                ((:file "parser"))))
+  :description "S-expression markup language."
+  :long-description
+  #.(uiop:read-file-string
+     (uiop:subpathname *load-pathname* "README.md"))
+  :in-order-to ((test-op (test-op parenml-test))))
---
+++ src/parser.lisp
@@ -0,0 +1,29 @@
+(in-package :cl-user)
+(defpackage parenml.parser
+  (:use :cl :esrap)
+  (:export :parse-string))
+(in-package :parenml.parser)
+
+(defrule whitespace (+ (or #\Space #\Tab #\Newline))
+  (:constant nil))
+
+(defrule atom (+ (not (or #\( #\))))
+  (:lambda (text)
+    (text text)))
+
+(defrule keyword (and #\: (+ (not (or #\( #\) whitespace))))
+  (:destructure (colon text)
+    (declare (ignore colon))
+    (intern (text text) :keyword)))
+
+(defrule list (and #\( (* sexp) #\))
+  (:destructure (open items close)
+    (declare (ignore open close))
+    items))
+
+(defrule sexp (and (or list keyword atom))
+  (:destructure (content)
+    content))
+
+(defun parse-string (string)
+  (parse 'sexp string))
