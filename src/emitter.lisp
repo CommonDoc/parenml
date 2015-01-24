a0af58642549d0fc3f3b4522aca15aad70315f62
Fernando Borretti <eudoxiahp@gmail.com>
2015-01-24 08:42:17 -0200

    Throw out most of the emitter

--- src/emitter.lisp
+++ src/emitter.lisp
@@ -4,24 +4,3 @@
   (:export :transform)
   (:documentation "Create an S-expression from a CommonDoc document."))
 (in-package :parenml.emitter)
-
-(defgeneric emit (node)
-  (:documentation "Create a ParenML representation of a CommonDoc document."))
-
-(defmethod emit ((list list))
-  "Emit a list."
-  (loop for elem in list do (emit elem)))
-
-(defmacro define-emitter (class &rest body)
-  "Define an emitter method."
-  `(defmethod emit ((node ,class))
-     ,@body))
-
-(defmacro define-child-emitter (class &rest body)
-  "Define a simple emitter for elements with `children`."
-  `(define-emitter ,class
-     (let ((children (emit (children node))))
-       ,@body)))
-
-(define-emitter <text-node>
-    (text node))
